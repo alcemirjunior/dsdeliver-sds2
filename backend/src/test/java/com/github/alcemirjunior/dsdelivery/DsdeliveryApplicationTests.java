a3f7c4c8f2608adfb4c79834a330fdbc664e3f36
alcemirjunior <alcemirmjunior@gmail.com>
2021-01-04 16:14:29 -0300

    project created

---
+++ backend/src/test/java/com/github/alcemirjunior/dsdelivery/DsdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alcemirjunior.dsdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
