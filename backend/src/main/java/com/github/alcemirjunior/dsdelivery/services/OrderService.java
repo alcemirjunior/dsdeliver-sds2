f45618cbf97a9bc83951d82ec041df4fb3221d44
alcemirjunior <alcemirmjunior@gmail.com>
2021-01-05 17:51:31 -0300

    post /orders

--- backend/src/main/java/com/github/alcemirjunior/dsdelivery/services/OrderService.java
+++ backend/src/main/java/com/github/alcemirjunior/dsdelivery/services/OrderService.java
@@ -1,12 +1,17 @@
 package com.github.alcemirjunior.dsdelivery.services;
 
 import com.github.alcemirjunior.dsdelivery.dto.OrderDTO;
+import com.github.alcemirjunior.dsdelivery.dto.ProductDTO;
 import com.github.alcemirjunior.dsdelivery.entities.Order;
+import com.github.alcemirjunior.dsdelivery.entities.OrderStatus;
+import com.github.alcemirjunior.dsdelivery.entities.Product;
 import com.github.alcemirjunior.dsdelivery.repositories.OrderRepository;
+import com.github.alcemirjunior.dsdelivery.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,6 +21,9 @@ public class OrderService {
     @Autowired
     private OrderRepository repository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Transactional(readOnly = true)
     public List<OrderDTO> findAll(){
         List<Order> list = repository.findOrdersWithProducts();
@@ -23,4 +31,17 @@ public class OrderService {
                 .map(x -> new OrderDTO(x)).collect(Collectors.toList());
     }
 
+    @Transactional
+    public OrderDTO insert(OrderDTO dto) {
+        Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
+                Instant.now(), OrderStatus.PENDING);
+        for(ProductDTO p : dto.getProducts()){
+            Product product = productRepository.getOne(p.getId()); //getOne instancia um produto, cria uma entidade gerenciada pelo jpa pra que quando salve o pedido, tmb salve as associações de quais produtos estao neste pedido
+            order.getProducts().add(product);
+        }
+        order = repository.save(order);
+        return new OrderDTO(order);
+    }
 }
+
+
